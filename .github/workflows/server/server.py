56eeec20fa938af268ba3d35be19df0bbfff0682
KandaGiGi <52697895+WerewolfwolfyXD@users.noreply.github.com>
2023-04-21 23:34:45 +0800

    Update server.py

--- .github/workflows/server/server.py
+++ .github/workflows/server/server.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from flask import Flask
+import os, sys
+
+app = Flask(__name__)
+
+@app.route("/flasktest")
+def route_flasktest():
+  return "It was worked!"
+
+if __name__ == "__main__":
+  app.run()
