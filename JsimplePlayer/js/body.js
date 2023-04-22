8091e048558ab0701737b4434a51f2a39cba26d2
KandaGiGi <52697895+WerewolfwolfyXD@users.noreply.github.com>
2023-04-22 16:58:34 +0800

    Add files via upload

---
+++ JsimplePlayer/js/body.js
@@ -0,0 +1,29 @@
+
+
+var songid = window.location.href.split("songid=")[1];
+const wyy_api = Array.of(["http://music.163.com/song/media/outer/url?id=", "http://music.163.com/api/song/detail/?id=${songid}&ids=%5B${songid}%5D", "http://cloud-music.pl-fe.cn/"]);
+
+
+
+
+
+fetch('http://cloud-music.pl-fe.cn/user/detail?uid=32953014', {
+    mode: "cors",
+    method: "GET",
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 1145141919810"
+    }
+})
+    .then(response => {
+        return response.text();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+
+document.getElementById("img_amb").src = "https://tse2-mm.cn.bing.net/th/id/OIP-C.aND-q9zGSuaDjbJ6l2pEDwHaE7?w=263&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7";
+document.getElementById("@div-info0-title_h2").innerText = wyy_api[0];
