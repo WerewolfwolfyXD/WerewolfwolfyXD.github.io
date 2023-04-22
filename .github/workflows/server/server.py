c8f9065503320651e48367cb174145f6a69f2046
KandaGiGi <52697895+WerewolfwolfyXD@users.noreply.github.com>
2023-04-22 15:55:02 +0800

    Update server.py

--- .github/workflows/server/server.py
+++ .github/workflows/server/server.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def route_flasktest():
   return "It was worked!"
 
+@app.route("/")
+def route_index():
+  return "Flask Server Was Running"
+
 if __name__ == "__main__":
-  app.run(port=80)
+  app.run(host="0.0.0.0",port=80)
